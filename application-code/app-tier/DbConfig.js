0d82e0fc30046b7347ad0492ff51b242c2d12e85
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-05-11 18:40:21 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'my3db1staging.czuuyk0ecdzx.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root1234',
+    DB_DATABASE : 'mydb'
 });
